18d49b9eb9a961ab21acaba24329a53a1741a7b8
excelitai-ashim <ashim@excelitai.com>
2023-01-10 14:38:49 +0600

    Done

---
+++ src/components/pages/Bonus.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="content-wrapper">
+  <div class="container-fluid">
+  <div class="row mx-2">
+
+
+   <div class="col-md-6 p-2"> 
+    <div class="form-container">
+    <form id="department-data">
+    
+      <div class="form-group">
+        <label for="exampleInputEmail1">Bonus Name</label>
+        <input type="text" class="form-control shadow-none" id="departmentName" name="departmentName"  >
+      </div>
+      
+      <button id="adddddd" type="submit" class="btn btn-primary">Add</button>
+    </form>
+  </div>
+   </div>
+   <div class="col-md-6 p-2">
+    <div class="table-container">
+    <table class="table table-bordered">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">SL</th>
+          <th scope="col">Bonus Name</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+      
+      </tbody>
+    </table>
+  </div>
+   </div>
+ </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BonusList',
+
+}
+</script>
+<style>
+ .form-container, .table-container{
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    background: #ffff;
+    margin: 10px 0px;
+    padding: 10px
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import {createRouter,createWebHistory} from "vue-router";
 import UserList from "../components/pages/UserList.vue";
 import Dashboard from "../components/pages/Dashboard.vue";
+import DeductionList from "../components/pages/Deduction.vue";
+import AllowanceList from "../components/pages/Allowance.vue";
+import BonusList from "../components/pages/Bonus.vue";
+import PositionList from "../components/pages/Position.vue";
+import DepartmentList from "../components/pages/Department.vue";
 const routes=[
     {
       path: "/user-list",
@@ -11,6 +16,31 @@ const routes=[
       path: "/",
       name:"dashboard",
       component:Dashboard
+    },
+    {
+      path: "/deduction",
+      name:"DeductionList",
+      component:DeductionList
+    },
+    {
+      path: "/allowance",
+      name:"AllowanceList",
+      component:AllowanceList
+    },
+    {
+      path: "/bonus",
+      name:"BonusList",
+      component:BonusList
+    },
+    {
+      path: "/position",
+      name:"PositionList",
+      component:PositionList
+    },
+    {
+      path: "/department",
+      name:"DepartmentList",
+      component:DepartmentList
     }
 ]
 export default createRouter({
